4c267a49bba0ba1c4c502e689035acf2ab7841ec
Markus Bordihn <Markus@Bordihn.de>
2025-08-17 00:05:15 +0200

    First release of reworked Glow Sticks mods for Fabric, Forge and NeoForge. General Code optimizations and improvements. Fixed #3 by allowing more combinations for crafting.

---
+++ Common/src/main/java/de/markusbordihn/glowsticks/item/GlowStickItem.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2021 Markus Bordihn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package de.markusbordihn.glowsticks.item;
+
+import de.markusbordihn.glowsticks.Constants;
+import de.markusbordihn.glowsticks.config.GlowSticksConfig;
+import de.markusbordihn.glowsticks.entity.projectile.GlowStickProjectile;
+import de.markusbordihn.glowsticks.utils.ToolTips;
+import java.util.List;
+import java.util.function.Supplier;
+import net.minecraft.ChatFormatting;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+
+public class GlowStickItem extends Item {
+
+  public static final String NAME = "glow_stick";
+  public static final String TAG_ACTIVATED = "activated";
+  public static final String TAG_STEP = "step";
+  public static final int ANIMATION_STEPS = 6;
+  public static final int DURATION_TICKS = ANIMATION_STEPS * 2;
+
+  protected final Supplier<Block> blockSupplier;
+  protected final boolean despawnEnabled;
+  protected final int despawnTickRate;
+  private final DyeColor dyeColor;
+
+  public GlowStickItem(Properties properties, Supplier<Block> blockSupplier, DyeColor dyeColor) {
+    this(
+        properties,
+        blockSupplier,
+        dyeColor,
+        GlowSticksConfig.glowStickDespawnEnabled,
+        GlowSticksConfig.glowStickDespawnTicks);
+  }
+
+  public GlowStickItem(
+      Properties properties,
+      Supplier<Block> blockSupplier,
+      DyeColor dyeColor,
+      boolean despawnEnabled,
+      int despawnTickRate) {
+    super(properties);
+    this.blockSupplier = blockSupplier;
+    this.despawnEnabled = despawnEnabled;
+    this.despawnTickRate = despawnTickRate;
+    this.dyeColor = dyeColor;
+  }
+
+  public static boolean isActivated(final ItemStack stack) {
+    CompoundTag tag = stack.getTag();
+    return tag != null && tag.getBoolean(TAG_ACTIVATED);
+  }
+
+  public static void setActivated(final ItemStack stack, final boolean activated) {
+    stack.getOrCreateTag().putBoolean(TAG_ACTIVATED, activated);
+  }
+
+  public static int getStep(final ItemStack itemStack) {
+    CompoundTag tag = itemStack.getTag();
+    return tag != null ? tag.getInt(TAG_STEP) : 0;
+  }
+
+  public static int setStep(final ItemStack itemStack, final int step) {
+    itemStack.getOrCreateTag().putInt(TAG_STEP, step);
+    return step;
+  }
+
+  public static int increaseStep(final ItemStack itemStack) {
+    return setStep(itemStack, getStep(itemStack) + 1);
+  }
+
+  public GlowStickProjectile getGlowStickEntity(final Level level, final LivingEntity entity) {
+    throw new UnsupportedOperationException("GlowStickItem.getGlowStickEntity() not implemented!");
+  }
+
+  public DyeColor getDyeColor() {
+    return dyeColor;
+  }
+
+  public boolean isDespawnEnabled() {
+    return despawnEnabled;
+  }
+
+  public int getDespawnTickRate() {
+    return despawnTickRate;
+  }
+
+  @Override
+  public int getUseDuration(ItemStack itemStack) {
+    return DURATION_TICKS;
+  }
+
+  @Override
+  public UseAnim getUseAnimation(ItemStack itemStack) {
+    return UseAnim.NONE;
+  }
+
+  @Override
+  public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+    ItemStack itemStack = player.getItemInHand(hand);
+    if (isActivated(itemStack)) {
+      if (!level.isClientSide) {
+        GlowStickProjectile entity = getGlowStickEntity(level, player);
+        entity.setItem(itemStack);
+        entity.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
+        level.addFreshEntity(entity);
+        player.getCooldowns().addCooldown(this, DURATION_TICKS);
+        itemStack.shrink(1);
+      }
+      setActivated(itemStack, false);
+      setStep(itemStack, 0);
+      player.awardStat(Stats.ITEM_USED.get(this));
+      return InteractionResultHolder.sidedSuccess(itemStack, level.isClientSide);
+    } else {
+      player.startUsingItem(hand);
+      return InteractionResultHolder.consume(itemStack);
+    }
+  }
+
+  @Override
+  public void onUseTick(Level level, LivingEntity livingEntity, ItemStack itemStack, int count) {
+    if (!level.isClientSide && despawnEnabled && count % despawnTickRate == 0) {
+      int step = increaseStep(itemStack);
+      if (step == 4) {
+        level.playSound(
+            null,
+            livingEntity.getX(),
+            livingEntity.getY(),
+            livingEntity.getZ(),
+            SoundEvents.TRIDENT_HIT,
+            SoundSource.NEUTRAL,
+            1.0F,
+            1.0F);
+      }
+      if (step > ANIMATION_STEPS) {
+        setStep(itemStack, 0);
+      }
+    }
+  }
+
+  @Override
+  public void releaseUsing(ItemStack itemStack, Level level, LivingEntity entity, int timeLeft) {
+    if (!isActivated(itemStack)) {
+      setStep(itemStack, 0);
+    }
+  }
+
+  @Override
+  public ItemStack finishUsingItem(ItemStack itemStack, Level level, LivingEntity entity) {
+    if (!level.isClientSide && !isActivated(itemStack)) {
+      setActivated(itemStack, true);
+    }
+    return itemStack;
+  }
+
+  @Override
+  public void appendHoverText(
+      ItemStack itemStack, Level level, List<Component> tooltipList, TooltipFlag tooltipFlag) {
+    ToolTips.addTooltip(
+        tooltipList,
+        new TranslatableComponent(Constants.TEXT_PREFIX + NAME + "_" + dyeColor + "_description")
+            .withStyle(ChatFormatting.GRAY));
+    ToolTips.addTooltip(
+        tooltipList,
+        new TranslatableComponent(Constants.TEXT_PREFIX + NAME + "_use", despawnTickRate * 15)
+            .withStyle(ChatFormatting.GREEN));
+  }
+}
---
+++ Fabric/src/main/java/de/markusbordihn/glowsticks/client/renderer/ClientRenderer.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2021 Markus Bordihn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package de.markusbordihn.glowsticks.client.renderer;
+
+import de.markusbordihn.glowsticks.Constants;
+import de.markusbordihn.glowsticks.block.ModBlocks;
+import de.markusbordihn.glowsticks.entity.ModEntity;
+import de.markusbordihn.glowsticks.item.GlowStickItem;
+import de.markusbordihn.glowsticks.item.ModItems;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.ThrownItemRenderer;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.ItemStack;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ClientRenderer {
+  public static final Logger log = LogManager.getLogger(Constants.LOG_NAME);
+
+  protected ClientRenderer() {}
+
+  public static void registerItemRenderer() {
+    log.info("{} Client Setup ...", Constants.LOG_REGISTER_PREFIX);
+
+    ResourceLocation animationStep = new ResourceLocation(Constants.MOD_ID, GlowStickItem.TAG_STEP);
+
+    // Register animation steps for all glow stick colors
+    for (DyeColor dyeColor : DyeColor.values()) {
+      ItemProperties.register(
+          ModItems.getGlowStickItem(dyeColor), animationStep, ClientRenderer::getStepFromTag);
+    }
+
+    // Register render layers for all glow stick blocks
+    for (DyeColor dyeColor : DyeColor.values()) {
+      BlockRenderLayerMap.INSTANCE.putBlock(
+          ModBlocks.getGlowStickBlock(dyeColor), RenderType.translucent());
+    }
+
+    // Glow Stick Light Blocks (cutout mip)
+    BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GLOW_STICK_LIGHT, RenderType.cutoutMipped());
+    BlockRenderLayerMap.INSTANCE.putBlock(
+        ModBlocks.GLOW_STICK_LIGHT_WATER, RenderType.cutoutMipped());
+  }
+
+  public static float getStepFromTag(
+      final ItemStack itemStack, final ClientLevel level, final LivingEntity living, final int id) {
+    if (itemStack.getItem() instanceof GlowStickItem && living != null) {
+      CompoundTag compoundTag = itemStack.getTag();
+      if (compoundTag != null) {
+        return compoundTag.getInt(GlowStickItem.TAG_STEP);
+      }
+    }
+    return 0.0F;
+  }
+
+  public static void registerRenderers() {
+    log.info("{} Client Renderer ...", Constants.LOG_REGISTER_PREFIX);
+
+    // Register renderer for all glow stick entity colors
+    for (DyeColor dyeColor : DyeColor.values()) {
+      EntityRendererRegistry.register(
+          ModEntity.getGlowStickEntity(dyeColor), ThrownItemRenderer::new);
+    }
+  }
+}
