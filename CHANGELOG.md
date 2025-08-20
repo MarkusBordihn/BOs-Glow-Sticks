d913581fe8433cf1444f5f5d98d748473e5311aa
Markus Bordihn <Markus@Bordihn.de>
2025-08-20 22:14:10 +0200

    Added basic particle effects for the Glow Sticks. Added waypoint feature which shows a waypoint to the player when holding a Glow Stick and sneaking. Added a new configuration option.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,13 @@ All notable changes to this project will be documented in this file.
 This change log includes the summarized changes.
 For the full changelog, please go to the [GitHub History][history] instead.
 
+### v7.1.0
+
+- Added basic particle effects for the Glow Sticks.
+- Added waypoint feature which shows a waypoint to the player when holding a Glow Stick and
+  sneaking.
+- Added a new configuration option.
+
 ### v7.0.0
 
 - First release of reworked Glow Sticks mods for Fabric, Forge and NeoForge.
--- Common/src/main/java/de/markusbordihn/glowsticks/item/GlowStickItem.java
+++ Common/src/main/java/de/markusbordihn/glowsticks/item/GlowStickItem.java
@@ -60,8 +60,8 @@ public class GlowStickItem extends Item {
         properties,
         blockSupplier,
         dyeColor,
-        GlowSticksConfig.glowStickDespawnEnabled,
-        GlowSticksConfig.glowStickDespawnTicks);
+        GlowSticksConfig.despawnEnabled,
+        GlowSticksConfig.despawnTicks);
   }
 
   public GlowStickItem(
@@ -183,7 +183,7 @@ public class GlowStickItem extends Item {
 
   @Override
   public void onUseTick(Level level, LivingEntity livingEntity, ItemStack itemStack, int count) {
-    if (!level.isClientSide && despawnEnabled && count % despawnTickRate == 0) {
+    if (!level.isClientSide && count % despawnTickRate == 0) {
       int step = increaseStep(itemStack);
       if (step == 4) {
         level.playSound(
