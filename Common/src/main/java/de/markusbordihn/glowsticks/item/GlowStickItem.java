ad3c5aa1e3ea26739e39e1b284e4b7cbc8532491
Markus Bordihn <Markus@Bordihn.de>
2025-08-20 22:14:10 +0200

    Added basic particle effects for the Glow Sticks. Added waypoint feature which shows a waypoint to the player when holding a Glow Stick and sneaking. Added a new configuration option.

--- Common/src/main/java/de/markusbordihn/glowsticks/item/GlowStickItem.java
+++ Common/src/main/java/de/markusbordihn/glowsticks/item/GlowStickItem.java
@@ -62,8 +62,8 @@ public class GlowStickItem extends Item {
         properties,
         blockSupplier,
         dyeColor,
-        GlowSticksConfig.glowStickDespawnEnabled,
-        GlowSticksConfig.glowStickDespawnTicks);
+        GlowSticksConfig.despawnEnabled,
+        GlowSticksConfig.despawnTicks);
   }
 
   public GlowStickItem(
@@ -152,7 +152,7 @@ public class GlowStickItem extends Item {
 
   @Override
   public void onUseTick(Level level, LivingEntity livingEntity, ItemStack itemStack, int count) {
-    if (!level.isClientSide && despawnEnabled && count % despawnTickRate == 0) {
+    if (!level.isClientSide && count % despawnTickRate == 0) {
       int step = increaseStep(itemStack);
       if (step == 4) {
         level.playSound(
